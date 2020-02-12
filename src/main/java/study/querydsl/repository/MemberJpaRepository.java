a5ad3cc540238a38eec27c28fcc39590c36b51fc
gracelove <govlmo91@gmail.com>
2020-02-13 00:39:47 +0900

    동적 쿼리와 성능 최적화 조회 - where절 파라미터 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -88,5 +90,42 @@ public class MemberJpaRepository {
                 .leftJoin(QMember.member.team, QTeam.team)
                 .where(builder)
                 .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        QMember.member.id.as("memberId"),
+                        QMember.member.username,
+                        QMember.member.age,
+                        QTeam.team.id.as("teamId"),
+                        QTeam.team.name.as("teamName")
+                ))
+                .from(QMember.member)
+                .leftJoin(QMember.member.team, QTeam.team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLog(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.hasText(username) ? QMember.member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return StringUtils.hasText(teamName) ? QTeam.team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? QMember.member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLog(Integer ageLoe) {
+        return ageLoe != null ? QMember.member.age.loe(ageLoe) : null;
     }
 }
